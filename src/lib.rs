621191eaa526624acdc08ad90ee1c67a46294753
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-07-16 22:45:54 +1000

    feat: parse a config file and watch directory specified

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,34 @@
 pub mod file_utils;
+pub mod config;
+
+use std::{path::Path, sync::mpsc};
+use notify::{Event, RecursiveMode, Result, Watcher};
+
+use crate::file_utils::was_modification;
+
+pub fn watch_directory(watch_dir: &str) -> Result<()> {
+    let (tx, rx) = mpsc::channel::<Result<Event>>();
+
+    // Use recommended_watcher() to automatically select the best implementation
+    // for your platform. The `EventHandler` passed to this constructor can be a
+    // closure, a `std::sync::mpsc::Sender`, a `crossbeam_channel::Sender`, or
+    // another type the trait is implemented for.
+    let mut watcher = notify::recommended_watcher(tx)?;
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(Path::new(watch_dir), RecursiveMode::Recursive)?;
+    // Block forever, printing out events as they come in
+    for res in rx {
+        match res {
+            Err(e) => println!("watch error: {:?}", e),
+            Ok(e) => {
+                if was_modification(e) {
+                    println!("Something in subdir was modified");
+                }
+            },
+        }
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,10 @@
-use notify::{Event, RecursiveMode, Result, Watcher, event::EventKind};
-use std::{path::Path, sync::mpsc};
+use watchers::{watch_directory, config::Config};
 
-use watchers::file_utils::was_modification;
+const CONFIG_PATH: &str = "./config/config.yml";
 
-const WATCH_DIR: &str = "./test";
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = Config::load(CONFIG_PATH)?;
+    watch_directory(&config.watch_dir)?;
 
-fn main() -> Result<()> {
-    let (tx, rx) = mpsc::channel::<Result<Event>>();
-
-    // Use recommended_watcher() to automatically select the best implementation
-    // for your platform. The `EventHandler` passed to this constructor can be a
-    // closure, a `std::sync::mpsc::Sender`, a `crossbeam_channel::Sender`, or
-    // another type the trait is implemented for.
-    let mut watcher = notify::recommended_watcher(tx)?;
-
-    // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes.
-    watcher.watch(Path::new(WATCH_DIR), RecursiveMode::Recursive)?;
-    // Block forever, printing out events as they come in
-    for res in rx {
-        match res {
-            Err(e) => println!("watch error: {:?}", e),
-            Ok(e) => {
-                if was_modification(e) {
-                    println!("Something in subdir was modified");
-                }
-            },
-        }
-    }
-
-    Ok(())
+    todo!()
 }
